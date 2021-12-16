2bd9db66b323a8c89db9c19164826233060d239e
Ubuntu <ubuntu@ip-172-31-2-45.us-west-1.compute.internal>
2021-12-16 00:52:53 +0000

    added info.json

--- class-updater.php
+++ class-updater.php
@@ -7,49 +7,33 @@ class Updater {
     public function __construct() {
     }
 
-    public static function load() {
-    }
-
-    
     /*
-     * $res empty at this step
      * $action 'plugin_information'
      * $args stdClass Object ( [slug] => woocommerce [is_ssl] => [fields] => Array ( [banners] => 1 [reviews] => 1 [downloaded] => [active_installs] => 1 ) [per_page] => 24 [locale] => en_US )
      */
-    function load( $res, $action, $args ){
-    
-        // do nothing if this is not about getting plugin information
-        if( 'plugin_information' !== $action ) {
-            return false;
-        }
-    
-        // do nothing if it is not our plugin
-        if( plugin_basename( __DIR__ ) !== $args->slug ) {
-            return false;
-        }
-    
+    function load($res, $action, $args) {
         // info.json is the file with the actual plugin information on your server
-        $remote = wp_remote_get( 
-            'https://rudrastyh.com/wp-content/uploads/updater/info.json', 
+        $remote = wp_remote_get(
+            'https://rudrastyh.com/wp-content/uploads/updater/info.json',
             array(
                 'timeout' => 10,
                 'headers' => array(
                     'Accept' => 'application/json'
-                ) 
+                )
             )
         );
-    
+
         // do nothing if we don't get the correct response from the server
-        if( 
-            is_wp_error( $remote )
-            || 200 !== wp_remote_retrieve_response_code( $remote )
-            || empty( wp_remote_retrieve_body( $remote ) 
-        ) {
-            return false;	
+        if (
+            is_wp_error($remote)
+            || 200 !== wp_remote_retrieve_response_code($remote)
+            || empty(wp_remote_retrieve_body($remote)) 
+            ) {
+            return false;
         }
-    
-        $remote = json_decode( wp_remote_retrieve_body( $remote ) );
-        
+
+        $remote = json_decode(wp_remote_retrieve_body($remote));
+
         $res = new stdClass();
         $res->name = $remote->name;
         $res->slug = $remote->slug;
@@ -70,22 +54,15 @@ class Updater {
         );
         // in case you want the screenshots tab, use the following HTML format for its content:
         // <ol><li><a href="IMG_URL" target="_blank"><img src="IMG_URL" alt="CAPTION" /></a><p>CAPTION</p></li></ol>
-        if( ! empty( $remote->sections->screenshots ) ) {
-            $res->sections[ 'screenshots' ] = $remote->sections->screenshots;
+        if (!empty($remote->sections->screenshots)) {
+            $res->sections['screenshots'] = $remote->sections->screenshots;
         }
-    
+
         $res->banners = array(
             'low' => $remote->banners->low,
             'high' => $remote->banners->high
         );
-        
+
         return $res;
-    
     }
-
-
-
 }
-
-
-
