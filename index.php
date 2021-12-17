ced624b7f80de01413bb0dddaa50a87774cd131a
Ubuntu <ubuntu@ip-172-31-2-45.us-west-1.compute.internal>
2021-12-17 04:43:38 +0000

    updated the version number and moved the requires inside actions.

--- index.php
+++ index.php
@@ -3,19 +3,17 @@
 /**
  * Plugin Name: PixelPAD Global Plugin
  * Date: Nov 2021
- * Version: 1.0.5
+ * Version: 1.0.6
  * Description: Files used in all pixelpad subdomains should be here. Maybe we should move the global plugin to the THEME in the future
  * Text Domain: global-plugin
  * Author: pixelpad.io
+ * //make sure you update the version number in the info.json file as well
  */
 
 
 
 define("GLOBAL_PLUGIN_URL",  plugin_dir_url(__FILE__));
 define("GLOBAL_PLUGIN_DIR",  plugin_dir_path(__FILE__));
-
-require_once(GLOBAL_PLUGIN_DIR . "class-updater.php");
-$updater = new \Updater();
 
 add_action("init", function () {
     /**
@@ -25,6 +23,7 @@ add_action("init", function () {
     require_once(GLOBAL_PLUGIN_DIR . "class-bloat.php");
     require_once(GLOBAL_PLUGIN_DIR . "class-rest.php");
     require_once(GLOBAL_PLUGIN_DIR . "class-style.php");
+    require_once(GLOBAL_PLUGIN_DIR . "class-updater.php");
     
 }, 1);
 
@@ -34,6 +33,8 @@ add_action("init", function () {
 add_action("rest_api_init", "REST::registerMeta");
 add_action("admin_menu", "Bloat::removeMenuItems");
 add_action("admin_head", "Style::load");
-
+add_action("admin_init", function(){
+    $updater = new \Updater();
+})
 
 
