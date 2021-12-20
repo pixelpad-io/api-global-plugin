7b15aa1101d4087f2cd38cd698c379a8210d2cfc
Ubuntu <ubuntu@ip-172-31-2-45.us-west-1.compute.internal>
2021-12-20 10:49:32 +0000

    udpated versions

--- index.php
+++ index.php
@@ -3,15 +3,13 @@
 /**
  * Plugin Name: PixelPAD Global Plugin
  * Date: Nov 2021
- * Version: 1.0.11
+ * Version: 1.0.13
  * Description: Files used in all pixelpad subdomains should be here. Maybe we should move the global plugin to the THEME in the future
  * Text Domain: global-plugin
  * Author: pixelpad.io
  * //make sure you update the version number in the info.json file as well
  * //to update this, update the global plugin and push to git. then pull changes into other subdomains
  */
-
-
 
 define("GLOBAL_PLUGIN_URL",  plugin_dir_url(__FILE__));
 define("GLOBAL_PLUGIN_DIR",  plugin_dir_path(__FILE__));
